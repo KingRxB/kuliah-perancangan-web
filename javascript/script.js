5b39ea5ce28f6096b74bbe8c97d155eb677c2f34
Risqi Akbar Marsudi <risqiakbar95@gmail.com>
2023-04-08 00:06:16 +0800

    Mulai dari awal

---
+++ javascript/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  // mengubah warna latar belakang tombol saat mouse mengarah ke tombol dan saat mouse meninggalkan tombol tersebut.
+  $("#button-contact").hover(
+    function () {
+      $(this).css("background-color", "white");
+    },
+    function () {
+      $(this).css("background-color", "");
+    }
+  );
+
+  //untuk menampilkan dan menyembunyikan menu dropdown saat tombol menu diklik dan saat mouse meninggalkan tombol tersebut.
+  const $menuButton = $("#menu");
+  $menuButton.on("click", () => {
+    $(".dropdown-menu").show();
+  });
+
+  $("#menu").on("mouseleave", () => {
+    $(".dropdown-menu").hide();
+  });
+});
